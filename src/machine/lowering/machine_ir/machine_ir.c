33ccca138c594716fb4d85749f4191dbc6b96e63
glazecats <1137056084@qq.com>
2026-05-10 21:38:25 +0000

    Add all-spill conservative diagnostic mode

--- src/machine/lowering/machine_ir/machine_ir.c
+++ src/machine/lowering/machine_ir/machine_ir.c
@@ -31,6 +31,12 @@ static int machine_ir_trace_enabled(void) {
     return flag && flag[0] != '\0' && strcmp(flag, "0") != 0;
 }
 
+static int machine_ir_aggressive_opt_mode_enabled(void) {
+    const char *flag = getenv("COMPILER_ENABLE_AGGRESSIVE_OPTIMIZATIONS");
+
+    return flag && flag[0] != '\0' && strcmp(flag, "0") != 0;
+}
+
 static void machine_ir_trace_timing(const char *stage, double elapsed_s) {
     if (!stage || !machine_ir_trace_enabled()) {
         return;
